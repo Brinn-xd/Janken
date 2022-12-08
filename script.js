5feace5ccf140f2c3baec66c5fb32a5167f9a973
Brinn-xd <bryan.moreno70@yahoo.com>
2022-12-08 03:18:25 -0600

    project functionally complete!

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const paaOp = document.getElementById("p");
 const chokiOp = document.getElementById("s");
 const userScore_span = document.getElementById("user-score");
 const botScore_span = document.getElementById("bot-score");
+const rButton = document.querySelector(".middle > button");
+const cards = document.querySelector(".choiceboard");
 
 function getComputerChoice() {
     const choices = ['r','p','s'];
@@ -15,20 +17,48 @@ function getComputerChoice() {
 function convertToWord(letter) {
   if (letter === "r") return "Guu";
   if (letter === "p") return "Paa";
-  return "Choki";
+  return "Choki"; 
+}
+function buttonPop() {
+  cards.style.pointerEvents = "none";
+  rButton.addEventListener('click', function (){
+    cards.style.pointerEvents = "all";
+    rButton.style.display = "none";
+    beginning.innerHTML = `BEGIN!`;
+    userScore = 0;
+    userScore_span.innerHTML = userScore;
+    botScore = 0;
+    botScore_span.innerHTML = botScore;
+  })
+  }
+
+function winCheck(userScore,botScore) {
+  if (userScore === 5) {
+    beginning.innerHTML = `GAME SET! YOU WIN!`;
+    rButton.style.display = "inline";
+    buttonPop();
+
+  }
+  else if (botScore === 5) {
+    beginning.innerHTML = `GAME SET! OPPONENT WINS!`;
+    rButton.style.display = "inline";
+    buttonPop();
+  }
 }
 
-function win(userChoice, computerChoice) {
+function win(userChoice,computerChoice) {
     userScore++;
     userScore_span.innerHTML = userScore;
     botScore_span.innerHTML = botScore;
     beginning.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}`
+    winCheck(userScore,botScore);
 }
 function loss(userChoice, computerChoice) {
   botScore++;
   userScore_span.innerHTML = userScore;
   botScore_span.innerHTML = botScore;
   beginning.innerHTML = `${convertToWord(userChoice)} loses to ${convertToWord(computerChoice)}`
+  winCheck(userScore,botScore);
 }
 function draw(userChoice, computerChoice) {
 beginning.innerHTML = `${convertToWord(userChoice)} equals ${convertToWord(computerChoice)}`
@@ -55,6 +85,7 @@ function game(userChoice) {
   }
 }
 game();
+
 function main() {
     guuOp.addEventListener('click', function(){
     game("r")
@@ -68,4 +99,5 @@ function main() {
     game("s");
     })
 }   
+
 main();
